360917161eb005dacd7b33b9f4aef6d8dc45bf9e
Amanthukral12 <47064923+Amanthukral12@users.noreply.github.com>
2020-05-11 22:55:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require("express"),
 	bodyParser=require("body-parser"),
 	mongoose = require("mongoose"),
 	Campground =require("./models/campground"),
+	Comment=require("./models/comment"),
 	seedDB= require("./seeds")
 
  
@@ -15,6 +16,7 @@ useNewUrlParser: true,
 app.use(bodyParser.urlencoded({extended: true})); 
 
 app.set("view engine","ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 
@@ -74,6 +76,24 @@ app.get("/campgrounds/:id/comments/new",function(req,res){
 	})
 	
 })
+app.post("/campgrounds/:id/comments", function(req,res) {
+	Campground.findById(req.params.id, function(err,campground) {
+		if(err){
+			console.log(err);
+			res.redirect("/campground");
+		} else{
+			Comment.create(req.body.comment, function(err,comment){
+				if(err){
+					console.log(err);
+				} else{
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			});
+		}
+	})
+});
 
 app.listen(port, function function_name() {
 	console.log("the yelpcamp server has started!");
