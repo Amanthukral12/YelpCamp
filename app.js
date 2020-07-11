450964c6a3b9c92e27709e149549d794927967cb
Aman Thukral <thukral618@gmail.com>
2020-07-11 16:36:14 +0530

    commit

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
 var express = require("express"),
 	app = express(),
-	bodyParser=require("body-parser"),
+	bodyParser = require("body-parser"),
 	mongoose = require("mongoose"),
-	flash=require("connect-flash"),
+	flash = require("connect-flash"),
 	passport = require("passport"),
-    LocalStrategy = require("passport-local"),
-    methodOverride= require("method-override"),
-    passportLocalMongoose = require("passport-local-mongoose"),
-    expressSession = require("express-session"),
-	Campground =require("./models/campground"),
-	Comment=require("./models/comment"),
-	User=require("./models/user");
-	seedDB= require("./seeds")
+	LocalStrategy = require("passport-local"),
+	methodOverride = require("method-override"),
+	passportLocalMongoose = require("passport-local-mongoose"),
+	expressSession = require("express-session"),
+	Campground = require("./models/campground"),
+	Comment = require("./models/comment"),
+	User = require("./models/user");
+seedDB = require("./seeds")
 
 var commentRoutes = require("./routes/comments"),
-	campgroundRoutes= require("./routes/campgrounds"),
-	indexRoutes= require("./routes/index")	
+	campgroundRoutes = require("./routes/campgrounds"),
+	indexRoutes = require("./routes/index")
 
 
 mongoose
-.connect("mongodb://localhost:27017/yelp_camp", {
-useUnifiedTopology: true,
-useNewUrlParser: true,
-});
-app.use(bodyParser.urlencoded({extended: true}));  
+	//.connect("mongodb://localhost:27017/yelp_camp", {
+	.connect("mongodb+srv://Aman:Abcd@123@yelpcamp.fdfv5.mongodb.net/<dbname>?retryWrites=true&w=majority", {
+		useUnifiedTopology: true,
+		useNewUrlParser: true,
+	});
 
-app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
@@ -44,10 +46,10 @@ passport.use(new LocalStrategy(User.authenticate()));//User.authenticate comes i
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req,res,next){
+app.use(function (req, res, next) {
 	res.locals.currentUser = req.user;
-	res.locals.error=req.flash("error");
-	res.locals.success=req.flash("success");
+	res.locals.error = req.flash("error");
+	res.locals.success = req.flash("success");
 	next();
 });
 
@@ -57,5 +59,5 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log("Server has started");
+	console.log("Server has started");
 });
